833331408b674def622fbb9b7affff85036ef884
martokk <25113632+martokk@users.noreply.github.com>
2022-10-12 16:06:30 -0500

    Adding zsh-completions and thefuck

--- install.sh
+++ install.sh
@@ -54,11 +54,12 @@ echo "--------------------------------- "
 printf "\nInstalling dependencies...\n"
 echo "--------------------------------- "
 if [ $SIMULATE == 1 ]; then
-    sudo apt install fontconfig zsh -y
+    sudo apt install fontconfig zsh thefuck -y
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
     git clone https://github.com/zsh-users/zsh-autosuggestions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+    git clone https://github.com/zsh-users/zsh-completions "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-completions"
 fi
 
 printf "\nCreating symlinks to dotfiles\n"
