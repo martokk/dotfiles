a6217d2b26b11c5e3fd0d5b549780d651204deb8
martokk <25113632+martokk@users.noreply.github.com>
2022-10-14 03:00:57 -0500

    Bugfix: adding sudo apt update

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ echo "--------------------------------- "
 printf "\nInstalling dependencies...\n"
 echo "--------------------------------- "
 if [ $SIMULATE == 1 ]; then
-    sudo apt install fontconfig zsh thefuck -y
+    sudo apt update && sudo apt install fontconfig zsh thefuck -y
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting"
